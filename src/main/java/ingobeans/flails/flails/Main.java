c922afaf388dcb58e22df4a411312530a62f16ef
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-05-24 22:37:22 +0200

    initial commit

---
+++ src/main/java/ingobeans/flails/flails/Main.java
@@ -0,0 +1,16 @@
+package ingobeans.flails.flails;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "flails";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+        ModItems.initialize();
+        LOGGER.info("flails loaded!!!!");
+	}
+}
---
+++ src/main/java/ingobeans/flails/flails/ModItems.java
@@ -0,0 +1,33 @@
+package ingobeans.flails.flails;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
+        // Create the item key.
+        Object ExampleMod;
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Main.MOD_ID, name));
+
+        // Create the item instance.
+        T item = itemFactory.apply(settings.setId(itemKey));
+
+        // Register the item.
+        Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+        return item;
+    }
+    public static void initialize() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.COMBAT)
+                .register((creativeTab) -> creativeTab.accept(ModItems.FLAIL));
+    }
+    public static final Item FLAIL = register("flail", Item::new, new Item.Properties());
+}
