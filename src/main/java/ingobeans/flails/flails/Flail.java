127a8bf32b505962430a1bf5ef029801bc0e5a95
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-05-30 15:15:52 +0200

    add flail item attribute

--- src/main/java/ingobeans/flails/flails/Flail.java
+++ src/main/java/ingobeans/flails/flails/Flail.java
@@ -5,15 +5,21 @@ import com.zigythebird.playeranim.api.PlayerAnimationAccess;
 import com.zigythebird.playeranimcore.animation.layered.modifier.SpeedModifier;
 import com.zigythebird.playeranimcore.api.firstPerson.FirstPersonConfiguration;
 import com.zigythebird.playeranimcore.api.firstPerson.FirstPersonMode;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityReference;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.component.UseEffects;
 import net.minecraft.world.level.Level;
 
 import static com.zigythebird.playeranim.PlayerAnimLibMod.ANIMATION_LAYER_ID;
@@ -25,6 +31,18 @@ public class Flail extends Item {
     public FlailHead activeHead;
     public float rotationsPerSecond = 0.8f;
     public float radius = 3.0f;
+
+    public static Properties createFlailProperties() {
+        return new Item.Properties().stacksTo(1)
+                .component(DataComponents.USE_EFFECTS, new UseEffects(true, true, 0.4F))
+                .attributes(ItemAttributeModifiers.builder()
+                        .add(
+                                Attributes.ATTACK_DAMAGE,
+                                new AttributeModifier(Item.BASE_ATTACK_DAMAGE_ID, 5.0, AttributeModifier.Operation.ADD_VALUE),
+                                EquipmentSlotGroup.MAINHAND
+                        )
+                .build());
+    }
 
     @Override
     public boolean releaseUsing(final ItemStack itemStack, final Level level, final LivingEntity entity, final int remainingTime) {
