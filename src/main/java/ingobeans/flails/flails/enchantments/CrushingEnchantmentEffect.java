c7c26dffdc6f6b80f62c567cbf1da496093a795d
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-06-05 18:05:10 +0200

    make crushing enchantment work

--- src/main/java/ingobeans/flails/flails/enchantments/CrushingEnchantmentEffect.java
+++ src/main/java/ingobeans/flails/flails/enchantments/CrushingEnchantmentEffect.java
@@ -10,7 +10,9 @@ import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.BlocksAttacks;
+import net.minecraft.world.item.component.Weapon;
 import net.minecraft.world.item.enchantment.EnchantedItemInUse;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.item.enchantment.effects.EnchantmentEntityEffect;
@@ -27,7 +29,13 @@ public record CrushingEnchantmentEffect(LevelBasedValue amount) implements Encha
     public void apply(ServerLevel serverLevel, int level, EnchantedItemInUse context, Entity target, Vec3 pos) {
         if (target instanceof LivingEntity victim) {
             if (context.owner() != null && context.owner() instanceof Player player) {
-                //BlocksAttacks blocksAttacks = victim.getItemBlockingWith().get(DataComponents.BLOCKS_ATTACKS);
+                ItemStack itemBlockingWith = victim.getItemBlockingWith();
+                if (itemBlockingWith != null) {
+                    BlocksAttacks blocksAttacks = itemBlockingWith.get(DataComponents.BLOCKS_ATTACKS);
+
+                    float secondsToDisableBlocking = Weapon.AXE_DISABLES_BLOCKING_FOR_SECONDS;
+                    blocksAttacks.disable(serverLevel, victim, secondsToDisableBlocking, itemBlockingWith);
+                }
             }
         }
     }
